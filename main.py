22e2ebb6c5a563cbcf353e4bf0d5dab4c7554e6f
yechanpark <yechanpark07@gmail.com>
2023-06-07 00:57:46 -0700

    added game_active, obstacles (spike wall) try the insanely hard level :) (i beat it)

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.image.load('graphics/playericon.png').convert_alpha()
         self.image = pygame.transform.rotozoom(self.image,0,0.05*scale)
         self.rect = self.image.get_rect(center = (360,360))
+        self.mask = pygame.mask.from_surface(self.image)
         self.speed = 10*scale
         self.shadows=[]
         self.spacedown=False
@@ -49,7 +50,7 @@ class Player(pygame.sprite.Sprite):
         if len(self.shadows) > 3:
             self.shadows = self.shadows[-3:]
         i=1
-        for shadow in self.shadows[::-1]:
+        for shadow in self.shadows:
             if i==1:
                 pygame.draw.circle(screen,'white',shadow,(self.rect.width*2/3))
             pygame.draw.circle(screen,'#4aa150',shadow,(self.rect.width)/(2+0.2*i))
@@ -57,7 +58,7 @@ class Player(pygame.sprite.Sprite):
             shadownum.text_blit()
             i+=1
 
-    def checkborder(self):
+    def check_border(self):
         if self.bordermode == 'bounded':
             if self.rect.left < 0:
                 self.rect.left = 0
@@ -81,7 +82,63 @@ class Player(pygame.sprite.Sprite):
     def update(self):
         self.putshadows()
         self.player_input()
-        self.checkborder()
+        self.check_border()
+
+class Obstacle(pygame.sprite.Sprite):
+    def __init__(self,type,speed,direction=None,bordermode='bounded'):
+        super().__init__()
+        self.type = type
+        if self.type == 'spikewall':
+            self.direction = direction
+            self.image = pygame.image.load('graphics/spikewall.png').convert_alpha()
+            self.image = pygame.transform.rotozoom(self.image,direction,1)
+            self.rect = self.image.get_rect(midbottom=(360,0))
+            self.mask = pygame.mask.from_surface(self.image)
+            if self.direction == 90:
+                self.rect.midright = (0,360)
+            elif self.direction == 180:
+                self.rect.midtop = (360,720)
+            elif self.direction == -90:
+                self.rect.midleft = (720,360)
+        self.speed = speed
+        self.bordermode = bordermode
+
+    def movement(self):
+        if self.direction == 0:
+            self.rect.y += self.speed
+        elif self.direction == 180:
+            self.rect.y -= self.speed
+        elif self.direction == 90:
+            self.rect.x += self.speed
+        elif self.direction == -90:
+            self.rect.x -= self.speed
+
+    #def check_border(self):
+     #   if self.bordermode == 'bounded':
+      #      if self.rect.right < 0:
+       #         self.rect.left = 0
+        #    elif self.rect.right > 720:
+         #       self.rect.right = 720
+          #  if self.rect.top < 0:
+           #     self.rect.top = 0
+            #elif self.rect.bottom > 720:
+             #   self.rect.bottom = 720
+    def check_border(self):
+        if self.bordermode == 'bounded':
+            if self.rect.right < 0 or self.rect.left > 720 or self.rect.top > 720 or self.rect.bottom < 0:
+                self.kill()
+        elif self.bordermode == 'unbounded':
+            if self.rect.right < 0:
+                self.rect.left = 720
+            elif self.rect.left > 720:
+                self.rect.right = 0
+            if self.rect.bottom < 0:
+                self.rect.top = 720
+            elif self.rect.top > 720:
+                self.rect.bottom = 0
+    def update(self):
+        self.check_border()
+        self.movement()
 
 class Text():
     def __init__(self,text,size,color,xpos,ypos,hover,key=None):
@@ -116,40 +173,70 @@ class Text():
         if self.hover:
             self.text_hover()
         screen.blit(self.image,self.rect)
-        
+
+def checkcollisions():
+    if pygame.sprite.spritecollide(player,obstacles,False,pygame.sprite.collide_mask):
+        player.shadows.clear()
+        obstacles.empty()
+        player.rect.center = (360,360)
+        return False
+    return True
+
+
 screen = pygame.display.set_mode((720,720))
 pygame.display.set_caption('Little Yellow')
 clock = pygame.time.Clock()
 game_active = False
-greenbg = pygame.image.load('graphics/greenbg.jpeg').convert()
-shadows=[]
 
 #-------------------------GROUPS--------------------------------------
-player = pygame.sprite.GroupSingle()
-player.add(Player())
+player_group = pygame.sprite.GroupSingle()
+player = Player()
+player_group.add(player)
+obstacles = pygame.sprite.Group()
+obstacles.add(Obstacle('spikewall',1,0,'unbounded'))
+obstacles.add(Obstacle('spikewall',2,90,'unbounded'))
+obstacles.add(Obstacle('spikewall',3,-90,'unbounded'))
+#obstacles.add(Obstacle('spikewall',4,180,'unbounded'))
+
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
             pygame.quit()
             exit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_d:
-        #        print('hi')
-                player.sprites()[0].shadows.append((player.sprites()[0].rect.centerx,player.sprites()[0].rect.centery))
-            if event.key == pygame.K_f and player.sprites()[0].shadows:
-                player.sprites()[0].rect.centerx = player.sprites()[0].shadows[-1][0]
-                player.sprites()[0].rect.centery = player.sprites()[0].shadows[-1][1]
-                player.sprites()[0].shadows.pop()
-    #background
-    screen.fill('#65db6d')
+        if game_active:
+            if event.type == pygame.KEYDOWN:
+                #create shadow
+                if event.key == pygame.K_d:
+                    player.shadows.append((player.rect.centerx,player.rect.centery))
+                #flash to shadow
+                if event.key == pygame.K_f and player.shadows:
+                    player.rect.centerx = player.shadows[0][0]
+                    player.rect.centery = player.shadows[0][1]
+                    player.shadows.pop(0)
+        else:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                game_active = True
+                obstacles.add(Obstacle('spikewall', 1, 0, 'unbounded'))
+                obstacles.add(Obstacle('spikewall', 2, 90, 'unbounded'))
+                obstacles.add(Obstacle('spikewall', 3, -90, 'unbounded'))
+                #obstacles.add(Obstacle('spikewall', 4, 180, 'unbounded'))
 
-    #player
-    player.update()
-    player.draw(screen)
+    if game_active:
+        #background
+        screen.fill('#65db6d')
 
+        #player
+        player.update()
+        player_group.draw(screen)
+        obstacles.update()
+        obstacles.draw(screen)
+
+        game_active = checkcollisions()
+    else:
+        screen.fill('#65db6d')
+        title_text = Text('Little Yellow',50,'yellow',360,300,False)
+        title_text.text_blit()
 
     clock.tick(60)
-
     pygame.display.update()
